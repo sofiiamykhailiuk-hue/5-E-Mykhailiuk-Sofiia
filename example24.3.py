89c4e062f43f3fc6d08a8569e8a13f5a586bf61c
sofiiamykhailiuk-hue <sofiiamykhailiuk@gmail.com>
2026-05-23 08:12:00 +0000

    Зробила пораграф 24 вправа 3

---
+++ example24.3.py
@@ -0,0 +1,27 @@
+from turtle import *
+
+Screen().setup(1.0, 1.0)
+delay(50)
+
+circle(50)
+
+up()
+goto(-20,60)
+down()
+
+dot(20)
+
+up()
+goto(20,60)
+down()
+
+dot(20)
+
+up()
+goto(20,40)
+down()
+
+left(90)
+circle(20,-160)
+
+done()
